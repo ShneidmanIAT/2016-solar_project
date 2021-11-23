e66571371d96b411ba8be30148bbcf092dbed163
ShneidmanIAT <crewmaster.crew@gmail.com>
2021-11-23 14:13:41 +0300

    Fixed the core of the programm, added reading from file.

--- solar_input.py
+++ solar_input.py
@@ -23,8 +23,10 @@ def read_space_objects_data_from_file(input_filename):
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
-            else:
-                print("Unknown space object")
+            elif object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
 
     return objects
 
@@ -43,8 +45,14 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
+    star.m = float(line.split()[3])
+    star.R = int(line.split()[1])
+    star.color = line.split()[2]
+    star.x = float(line.split()[4])
+    star.y = float(line.split()[5])
+    star.vx = float(line.split()[6])
+    star.vy = float(line.split()[7])
 
-    pass  # FIXME: not done yet
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,7 +69,13 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    planet.m = float(line.split()[3])
+    planet.R = int(line.split()[1])
+    planet.color = line.split()[2]
+    planet.x = float(line.split()[4])
+    planet.y = float(line.split()[5])
+    planet.vx = float(line.split()[6])
+    planet.vy = float(line.split()[7])
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
